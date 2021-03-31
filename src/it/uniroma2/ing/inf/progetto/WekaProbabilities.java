24eb8c32f9b99b457d35822a61f4972d121e92a3
Simone <39402200+simesi@users.noreply.github.com>
2021-03-31 17:26:40 +0200

    Delete of useless files

--- src/it/uniroma2/ing/inf/progetto/WekaProbabilities.java
+++ src/it/uniroma2/ing/inf/progetto/WekaProbabilities.java
@@ -7,67 +7,67 @@ import weka.core.converters.ConverterUtils.DataSource;
 
 
 public class WekaProbabilities {
-	public static void main(String args[]) throws Exception{
-		// load datasets
-		Instances training = DataSource.read("C:/Program Files/Weka-3-8/data/breast-cancerKnown.arff");
-		Instances testing = DataSource.read("C:/Program Files/Weka-3-8/data/breast-cancerNOTK.arff");
-		int numAttr = training.numAttributes();
-		training.setClassIndex(numAttr - 1);
-		testing.setClassIndex(numAttr - 1);
-		// make sure they're compatible
-		String msg = training.equalHeadersMsg(testing);
-		if (msg != null)
-			throw new Exception(msg);
 
-		int numtesting = testing.numInstances();
-		System.out.printf("There are %d test instances\n", numtesting);
+	// load datasets
+	Instances training = DataSource.read("C:/Program Files/Weka-3-8/data/breast-cancerKnown.arff");
+	Instances testing = DataSource.read("C:/Program Files/Weka-3-8/data/breast-cancerNOTK.arff");
+	int numAttr = training.numAttributes();
+	training.setClassIndex(numAttr - 1);
+	testing.setClassIndex(numAttr - 1);
+	// make sure they're compatible
+	String msg = training.equalHeadersMsg(testing);
+	if (msg != null)
+		throw new Exception(msg);
 
-		RandomForest classifier = new RandomForest();
-		classifier.buildClassifier(training);
+	int numtesting = testing.numInstances();
+	System.out.printf("There are %d test instances\n", numtesting);
 
-		// Loop over each test instance.
-		for (int i = 0; i < numtesting; i++)
+	RandomForest classifier = new RandomForest();
+	classifier.buildClassifier(training);
+
+	// Loop over each test instance.
+	for (int i = 0; i < numtesting; i++)
+	{
+		// Get the true class label from the instance's own classIndex.
+		String trueClassLabel = 
+				testing.instance(i).toString(testing.classIndex());
+
+		// Make the prediction here.
+		double predictionIndex = 
+				classifier.classifyInstance(testing.instance(i)); 
+
+		// Get the predicted class label from the predictionIndex.
+		String predictedClassLabel =
+				testing.classAttribute().value((int) predictionIndex);
+
+		// Get the prediction probability distribution.
+		double[] predictionDistribution = 
+				classifier.distributionForInstance(testing.instance(i)); 
+
+		// Print out the true label, predicted label, and the distribution.
+		System.out.printf("%5d: true=%-10s, predicted=%-10s, distribution=", 
+				i, trueClassLabel, predictedClassLabel); 
+
+		// Loop over all the prediction labels in the distribution.
+		for (int predictionDistributionIndex = 0; 
+				predictionDistributionIndex < predictionDistribution.length; 
+				predictionDistributionIndex++)
 		{
-			// Get the true class label from the instance's own classIndex.
-			String trueClassLabel = 
-					testing.instance(i).toString(testing.classIndex());
+			// Get this distribution index's class label.
+			String predictionDistributionIndexAsClassLabel = 
+					testing.classAttribute().value(
+							predictionDistributionIndex);
 
-			// Make the prediction here.
-			double predictionIndex = 
-					classifier.classifyInstance(testing.instance(i)); 
+			// Get the probability.
+			double predictionProbability = 
+					predictionDistribution[predictionDistributionIndex];
 
-			// Get the predicted class label from the predictionIndex.
-			String predictedClassLabel =
-					testing.classAttribute().value((int) predictionIndex);
+			System.out.printf("[%10s : %6.3f]", 
+					predictionDistributionIndexAsClassLabel, 
+					predictionProbability );
+		}
+		System.out.printf("\n");
 
-			// Get the prediction probability distribution.
-			double[] predictionDistribution = 
-					classifier.distributionForInstance(testing.instance(i)); 
-
-			// Print out the true label, predicted label, and the distribution.
-			System.out.printf("%5d: true=%-10s, predicted=%-10s, distribution=", 
-					i, trueClassLabel, predictedClassLabel); 
-
-			// Loop over all the prediction labels in the distribution.
-			for (int predictionDistributionIndex = 0; 
-					predictionDistributionIndex < predictionDistribution.length; 
-					predictionDistributionIndex++)
-			{
-				// Get this distribution index's class label.
-				String predictionDistributionIndexAsClassLabel = 
-						testing.classAttribute().value(
-								predictionDistributionIndex);
-
-				// Get the probability.
-				double predictionProbability = 
-						predictionDistribution[predictionDistributionIndex];
-
-				System.out.printf("[%10s : %6.3f]", 
-						predictionDistributionIndexAsClassLabel, 
-						predictionProbability );
-			}
-			System.out.printf("\n");
-		}    
 
 	}
 }
